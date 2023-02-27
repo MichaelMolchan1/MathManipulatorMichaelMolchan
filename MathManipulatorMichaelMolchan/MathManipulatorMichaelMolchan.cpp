651d320e28ce07c014df0012657ef54ebd7a4d35
Molch <molchaml@mail.uc.edu>
2023-02-27 14:02:03 -0500

    Using char instead of tokens

--- MathManipulatorMichaelMolchan/MathManipulatorMichaelMolchan.cpp
+++ MathManipulatorMichaelMolchan/MathManipulatorMichaelMolchan.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <cstring>
 
 using namespace std;
 
-// Function that divides the porgram into individual elements
-vector<string> splitExpression(string expression) {
-    vector<string> result; // create a vector that holds the individual elements
-    stringstream ss(expression); // create a stringstream to parse the expression
-    string token; // create a string variable to hold each individual element
-    while (ss >> token) { // loop through the stringstream, extracting each token
-        result.push_back(token); // add the token to the result vector
-    }
-    return result; // return the vector of individual elements
-}
 
-// Function to print the elements of a vector, one per line
-void printVector(vector<string> v) {
-    for (string element : v) {
-        cout << element << endl; // print each element followed by a newline
+// Function to split a C-string expression into individual elements (numbers, operators, and spaces)
+void splitExpression(char expression[], char* elements[], int& count) {
+    char* token = strtok(expression, " "); // get the first token
+    count = 0;
+    while (token != nullptr) { // loop through the tokens
+        elements[count] = token; // store the token in the array
+        count++;
+        token = strtok(nullptr, " "); // get the next token
     }
 }
 
-int main() {
-    string expression; // declare a variable to hold the user's input
-    while (true) { // repeat indefinitely until the user enters "0"
-        cout << "Enter a mathematical expression (enter 0 to exit): ";
-        getline(cin, expression); // read a line of input from the user
-        if (expression == "0") { // if the input is "0", break out of the loop
-            break;
-        }
-        vector<string> elements = splitExpression(expression); // parse the expression into individual elements
-        cout << "Original expression: " << expression << endl;
-        cout << "Elements of the expression: " << endl;
-        printVector(elements); // print the individual elements of the expression
-        cout << "Elements of the expression, reversed: " << endl;
-        reverse(elements.begin(), elements.end()); // reverse the order of the elements
-        printVector(elements); // print the reversed elements of the expression
+// Function to print the elements of a C-string array, one per line
+void printArray(char* elements[], int count) {
+    for (int i = 0; i < count; i++){
+        cout << elements[i] << endl;
     }
-    return 0; // exit the program
 }
+    
+    
